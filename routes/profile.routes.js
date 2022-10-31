975628aadd389ea8b6c48e213768d35d83ea32ec
Mog Rouhi <moghadase.roohi@gmail.com>
2022-10-31 20:33:16 +0000

    add basic css

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -5,41 +5,41 @@ const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
 const fileUploader = require("../config/cloudinary.config");
 
-router.put(
-  "/user/:userId/",
-  fileUploader.single("image"),
-  (req, res, next) => {
-    // if (req.file.path) {
-    //   const profileImg = req.file.path;
-    //   return profileImg;
-    // }
-    const {userId} = req.params;
-    const { headline, basedIn, technologies, githubUrl, screenshoot } = req.body;
+router.put("/user/:userId/", fileUploader.single("image"), (req, res, next) => {
+  // if (req.file.path) {
+  //   const profileImg = req.file.path;
+  //   return profileImg;
+  // }
+  const { userId } = req.params;
+  const { headline, basedIn, technologies, githubUrl, screenshoot } = req.body;
 
-    const newProfile = {
-      headline,
-      basedIn,
-      technologies,
-      githubUrl,
-      screenshoot,
-    };
-    console.log(userId)
-    console.log(newProfile)
+  const newProfile = {
+    headline,
+    basedIn,
+    technologies,
+    githubUrl,
+    screenshoot,
+  };
+  console.log(userId);
+  console.log(newProfile);
 
-    User.findByIdAndUpdate(userId, { profile: newProfile} ,{ returnDocument: "after" })
-      .then((newProfile) => {
-        console.log(newProfile)
-        res.json(newProfile);
-      })
-      .catch((err) => {
-        console.log("error creating new profile");
-        res.status(500).json({
-          message: "error creating a new profile",
-          error: err,
-        });
+  User.findByIdAndUpdate(
+    userId,
+    { profile: newProfile },
+    { returnDocument: "after" }
+  )
+    .then((newProfile) => {
+      console.log(newProfile);
+      res.json(newProfile);
+    })
+    .catch((err) => {
+      console.log("error creating new profile");
+      res.status(500).json({
+        message: "error creating a new profile",
+        error: err,
       });
-  }
-);
+    });
+});
 
 router.get("/user/:userId", (req, res, next) => {
   const { userId } = req.params;
@@ -51,11 +51,11 @@ router.get("/user/:userId", (req, res, next) => {
 
   User.findById(userId)
     .then((user) => {
-     // if (req.payload._id == userId) {
-        res.json(user);
-     // } else {
+      // if (req.payload._id == userId) {
+      res.json(user);
+      // } else {
       //  res.status(400).json({ message: "Not authorize to see this profile" });
-     // }
+      // }
     })
     .catch((err) => {
       console.log("error getting user");
@@ -70,34 +70,35 @@ router.put("/user/:userId", (req, res, next) => {
   const { userId } = req.params;
   const { headline, basedIn, technologies, githubUrl, screenshoot } = req.body;
 
-    const updatedProfile = {
-      headline,
-      basedIn,
-      technologies,
-      githubUrl,
-      screenshoot,
-    };
+  const updatedProfile = {
+    headline,
+    basedIn,
+    technologies,
+    githubUrl,
+    screenshoot,
+  };
 
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-      res.status(400).json({ message: 'Specified profile id is not valid' });
-      return;
-  } 
-  Profile.findByIdAndUpdate( userId, updatedProfile, { returnDocument: "after" } )
-  .then((updatedProfile) => {
-      if(req.payload._id == userId){
-          res.json(updatedProfile)
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    res.status(400).json({ message: "Specified profile id is not valid" });
+    return;
+  }
+  Profile.findByIdAndUpdate(userId, updatedProfile, { returnDocument: "after" })
+    .then((updatedProfile) => {
+      if (req.payload._id == userId) {
+        res.json(updatedProfile);
       } else {
-          res.status(400).json({message: 'Not authorize to update this profile'})
+        res
+          .status(400)
+          .json({ message: "Not authorize to update this profile" });
       }
-  })
-  .catch((err) => {
+    })
+    .catch((err) => {
       console.log("error updating the profile", userId);
       res.status(500).json({
         message: `error updating the profile ${userId} `,
         error: err,
       });
     });
-  
-})
+});
 
 module.exports = router;
