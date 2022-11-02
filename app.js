2bdc1093f6b104c90550b08ea627d61801972d5a
Alessandra Scarpellini <scaalessandra@icloud.com>
2022-11-02 18:12:58 +0100

    add socket.io

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use("/api", require("./routes/project.routes"));
 app.use("/api", require("./routes/profile.routes"));
 
 
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- server.js
+++ server.js
@@ -3,6 +3,43 @@ const app = require("./app");
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
 
-app.listen(PORT, () => {
+
+
+
+/////////////////////////////////////
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server,{
+  cors: {
+    //credentials: true,
+    origin: process.env.ORIGIN || "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+  
+  socket.on("send_message", (data) => {
+    console.log(data)
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+
+////////////////////////////////////
+
+
+server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
